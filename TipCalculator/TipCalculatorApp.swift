958621f00a1c82f6c9d489215984d5c697b4fa28
Maria Sandu <Maria.Sandu@emids.com>
2023-09-22 20:47:38 +0300

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Maria Sandu on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
